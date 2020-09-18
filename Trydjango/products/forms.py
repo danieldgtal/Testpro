06c6cfbf7cc443b708605aedaf93660f8971a7f5
unknown <danielchiatuiro@gmail.com>
2020-09-18 10:26:23 +0100

    18/09/2020 commit

--- Trydjango/products/forms.py
+++ Trydjango/products/forms.py
@@ -1,19 +1,27 @@
 from django import forms
 
+
 from .models import Product
 
+
 class ProductForm(forms.ModelForm):
-    title       = forms.CharField(label="", widget=forms.TextInput(attrs={"placeholder":"Your title"}))
-    description = forms.CharField(widget=forms.Textarea(attrs={
-                                "placeholder": "Your Description",
+    title        = forms.CharField(label='',
+                    widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    description  = forms.CharField(
+                        required=False,
+                        widget=forms.Textarea(
+                            attrs={
+                                "placeholder": "Your description",
                                 "class": "new-class-name two",
-                                "rows": 20,
+                                "class": "new-class-name two",
                                 "id": "my-id-for-textarea",
-                                "cols": "50"
+                                "rows": 20,
+                                'cols': 120
                             }
                         )
                     )
     price = forms.DecimalField(initial=199.99)
+
     # Email = forms.EmailField() wanted to do form validation here
 
     class Meta:
@@ -21,37 +29,43 @@ class ProductForm(forms.ModelForm):
         fields = [
             'title',
             'description',
-            'price',
+            'price'
         ]
-    # Form Validations
 
-    def clean_title(self, *args, **kwargs):
-        #title multiple validation
+   # Form Validations
+   #
+   #  def clean_title(self, *args, **kwargs):
+   #      # title multiple validation
+   #
+   #      title = self.cleaned_data.get("title")
+        # if "daniel" in title:
+        #     return title
+        # elif "New Product" in title:
+        #     return title
+        # else:
+        #     raise forms.ValidationError("Invalid")
+    #
+    # def clean_description(self, *args, **kwargs):
+    #     description = self.cleaned_data.get("description")
+    #     if len(description) <= 20:
+    #         raise forms.ValidationError("You need up to 20 characters")
+    #     else:
+    #         return description
 
-        title = self.cleaned_data.get("title")
-        if "daniel" in title:
-            return title
-        elif "New Product" in title:
-            return title
-        else:
-            raise forms.ValidationError("Invalid")
-
-
-    def clean_description(self, *args, **kwargs):
-        description = self.cleaned_data.get("description")
-        if len(description) <= 20:
-            raise forms.ValidationError("You need up to 20 characters")
-        else:
-            return description
 
 
 class RawProductForm(forms.Form):
-    title       = forms.CharField(label="", widget=forms.TextInput(attrs={"placeholder":"Your title"}))
-    description = forms.CharField(widget=forms.Textarea(attrs={
-                    "placeholder": "Your Description",
-                    "class": "new-class-name two",
-                    "rows" : 20,
-                    "id"   : "my-id-for-textarea",
-                    "cols" : "50"
-                }))
-    price       = forms.DecimalField()
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    description = forms.CharField(
+                        required=False,
+                        widget=forms.Textarea(
+                                attrs={
+                                    "placeholder": "Your description",
+                                    "class": "new-class-name two",
+                                    "id": "my-id-for-textarea",
+                                    "rows": 20,
+                                    'cols': 120
+                                }
+                            )
+                        )
+price   = forms.DecimalField(initial=199.99)
